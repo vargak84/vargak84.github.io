cde22a752f6d7e2409d17d983d6f23dde22dfcbf
vargak84 <einstein.xxx@gmail.com>
2020-07-21 11:56:20 +0200

    new

--- js/kopapirollo.js
+++ js/kopapirollo.js
@@ -9,6 +9,8 @@ function szabalyok() {
     window.alert("Agymenők kő-papír-olló szabályai:\n\nAz olló elvágja a papírt.\nA papír bevonja a követ.\nA kő agyonüti a gyíkot.\nA gyík megmarja Spockot.\nSpock eltöri az ollót.\nAz olló lefejezi a gyíkot.\nA gyík megeszi a papírt.\nA papír cáfolja Spockot.\nSpock feloldja a követ.\nÉs mint általában, a kő eltöri az ollót.");
 }
 
+
+
 //Játékos képe
 let jatekosKep = document.querySelector("small[id='jatekosKep']");
 let jatekosEredmeny = document.querySelector("small[id='jatekosEredmeny']");
@@ -19,11 +21,23 @@ let gepKep = document.querySelector("small[id='gepKep']");
 let gepEredmeny = document.querySelector("small[id='gepEredmeny']");
 
 //Játék gomb
-function jatek() {
-    minszam = 0;
-    maxszam = 4;
-    let veletlenszam1 = minszam + Math.floor( Math.random() * ( maxszam - minszam + 1 ) );
-    let veletlenszam2 = minszam + Math.floor( Math.random() * ( maxszam - minszam + 1 ) );
-    jatekosKep.innerHTML = veletlenszam1;
-    gepKep.innerHTML = veletlenszam2;
+function koValaszt() {
+    document.jatekosKep.src = document.vkokep.src;
+    document.vkokep.src
+}
+
+function papirValaszt() {
+    document.jatekosKep.src = document.vpapirkep.src;
+}
+
+function olloValaszt() {
+    document.jatekosKep.src = document.vollokep.src;
+}
+
+function gyikValaszt() {
+    document.jatekosKep.src = document.vgyikkep.src;
+}
+
+function spockValaszt() {
+    document.jatekosKep.src = document.vspockkep.src;
 }
