295c8f8d0d437d3bbd0eb2b5fffdd6cb8fff1508
vargak84 <einstein.xxx@gmail.com>
2020-07-22 06:54:19 +0200

    new

--- js/kopapirollo.js
+++ js/kopapirollo.js
@@ -9,35 +9,49 @@ function szabalyok() {
     window.alert("Agymenők kő-papír-olló szabályai:\n\nAz olló elvágja a papírt.\nA papír bevonja a követ.\nA kő agyonüti a gyíkot.\nA gyík megmarja Spockot.\nSpock eltöri az ollót.\nAz olló lefejezi a gyíkot.\nA gyík megeszi a papírt.\nA papír cáfolja Spockot.\nSpock feloldja a követ.\nÉs mint általában, a kő eltöri az ollót.");
 }
 
-
-
-//Játékos képe
-let jatekosKep = document.querySelector("small[id='jatekosKep']");
-let jatekosEredmeny = document.querySelector("small[id='jatekosEredmeny']");
-
-
-//Gép képe
-let gepKep = document.querySelector("small[id='gepKep']");
-let gepEredmeny = document.querySelector("small[id='gepEredmeny']");
-
 //Játék gomb
 function koValaszt() {
     document.jatekosKep.src = document.vkokep.src;
-    document.vkokep.src
+    veletlenKep();
+    kiNyer()
 }
 
 function papirValaszt() {
     document.jatekosKep.src = document.vpapirkep.src;
+    veletlenKep()
 }
 
 function olloValaszt() {
     document.jatekosKep.src = document.vollokep.src;
+    veletlenKep()
 }
 
 function gyikValaszt() {
     document.jatekosKep.src = document.vgyikkep.src;
+    veletlenKep()
 }
 
 function spockValaszt() {
     document.jatekosKep.src = document.vspockkep.src;
+    veletlenKep()
+}
+
+//Véletlenkép generátor
+function veletlenKep() {
+    let kep = new Array()
+    kep[1]="../img/ko.jpg"
+    kep[2]="../img/papir.jpg"
+    kep[3]="../img/ollo.jpg"
+    kep[4]="../img/gyik.jpg"
+    kep[5]="../img/spock.jpg"
+    
+    let ry=Math.floor(Math.random()*kep.length)
+    if (ry==0) 
+    ry=1
+    document.gepKep.src = kep[ry]
+    }
+
+function kiNyer() {
+    if (document.gepKep.src == document.jatekosKep.src)
+    console.log("döntetlen");
 }
