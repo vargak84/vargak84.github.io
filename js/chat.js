55669639dc95b98fc7650087f200e2e82f13b9c1
vargak84 <einstein.xxx@gmail.com>
2020-08-01 12:03:13 +0200

    new

--- js/chat.js
+++ js/chat.js
@@ -1,10 +1,26 @@
 let kuldesGomb = document.querySelector("#kuldesGomb");
 let kuldesMezo = document.querySelector("#kuldesMezo");
-let idMezo = document.querySelector("#idMezo");
+let idMezo = document.querySelectorAll("#idMezo");
 let szovegMezo = document.querySelector("#szovegMezo");
 
 function kuldes() {
     let szoveg = kuldesMezo.value;
     console.log(szoveg);
-    console.log(szovegMezo.value);
+
+    let message = {
+        message: kuldesMezo.value
+      };
+      let fetchOptions = {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify( message )
+      };
+      fetch( "http://localhost:3000/message/", fetchOptions )
+        .then( resp => resp.json() )
+        .then( json => console.log(json) );
 }
