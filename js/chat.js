7d93daaa3f5fb5788a18ec2e96b6cc22cd25150a
vargak84 <einstein.xxx@gmail.com>
2020-08-01 10:02:58 +0200

    new

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+let kuldesGomb = document.querySelector("#kuldesGomb");
+let kuldesMezo = document.querySelector("#kuldesMezo");
+let idMezo = document.querySelector("#idMezo");
+let szovegMezo = document.querySelector("#szovegMezo");
+
+function kuldes() {
+    let szoveg = kuldesMezo.value;
+    console.log(szoveg);
+}
