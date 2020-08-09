fbb84b7b2a8027dfa2a26c53aa96b7f6b158cd24
vargak84 <einstein.xxx@gmail.com>
2020-08-09 06:57:26 +0200

    new

--- js/countDownTimer.js
+++ js/countDownTimer.js
@@ -2,7 +2,7 @@ let elrejt1 = document.querySelector("#elrejt1");
 let elrejt2 = document.querySelector("#elrejt2");
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 9, 2020 06:57:00").getTime();
+var countDownDate = new Date("Aug 9, 2020 06:59:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
