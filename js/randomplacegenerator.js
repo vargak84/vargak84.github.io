a090d92e4e035d2165b998b22186520436289995
vargak84 <einstein.xxx@gmail.com>
2020-08-21 18:38:12 +0200

    new

---
+++ js/randomplacegenerator.js
@@ -0,0 +1,53 @@
+let digitOne = document.querySelector("#digitOne");
+let digitTwo = document.querySelector("#digitTwo");
+let digitThree = document.querySelector("#digitThree");
+let digitFour = document.querySelector("#digitFour");
+let digitFive = document.querySelector("#digitFive");
+let digitSix = document.querySelector("#digitSix");
+let digitSeven = document.querySelector("#digitSeven");
+let digitEight = document.querySelector("#digitEight");
+let digitNine = document.querySelector("#digitNine");
+let digitTen = document.querySelector("#digitTen");
+let digitAll = document.querySelectorAll(".digit");
+
+function generate() {
+
+    for (let i = 0; i < digitAll.length; i++) {
+        digitAll[i].value = 0;
+    };
+
+    minszam = 0;
+    maxszam = 9;
+    let randomNumber = minszam + Math.floor(Math.random() * (maxszam - minszam + 1));
+
+    if (randomNumber == 0) {
+        digitOne.value = 1;
+    } else
+        if (randomNumber == 1) {
+            digitTwo.value = 1;
+        } else
+            if (randomNumber == 2) {
+                digitThree.value = 1;
+            } else
+                if (randomNumber == 3) {
+                    digitFour.value = 1;
+                } else
+                    if (randomNumber == 4) {
+                        digitFive.value = 1;
+                    } else
+                        if (randomNumber == 5) {
+                            digitSix.value = 1;
+                        } else
+                            if (randomNumber == 6) {
+                                digitSeven.value = 1;
+                            } else
+                                if (randomNumber == 7) {
+                                    digitEight.value = 1;
+                                } else
+                                    if (randomNumber == 8) {
+                                        digitNine.value = 1;
+                                    } else
+                                        if (randomNumber == 9) {
+                                            digitTen.value = 1;
+                                        }
+};
---
+++ js/szinozon.js
@@ -0,0 +1,84 @@
+let megoldasElsoGolyoMezo = document.querySelector("#megoldasElsoGolyoMezo")
+let megoldasMasodikGolyoMezo = document.querySelector("#megoldasMasodikGolyoMezo")
+let megoldasHarmadikGolyoMezo = document.querySelector("#megoldasHarmadikGolyoMezo")
+let megoldasNegyedikGolyoMezo = document.querySelector("#megoldasNegyedikGolyoMezo")
+
+let pirosGolyo = document.querySelector("#pirosGolyo");
+let feherGolyo = document.querySelector("#feherGolyo");
+let kekGolyo = document.querySelector("#kekGolyo");
+let rozsaGolyo = document.querySelector("#rozsaGolyo");
+let narancsGolyo = document.querySelector("#narancsGolyo");
+let lilaGolyo = document.querySelector("#lilaGolyo");
+let sargaGolyo = document.querySelector("#sargaGolyo");
+let zoldGolyo = document.querySelector("#zoldGolyo");
+let kep;
+let ry;
+let rejt1 = document.querySelector(".table2");
+let rejt2 = document.querySelector("#rejt2");
+let rejt3 = document.querySelector("#ellenorzesGomb");
+let hozzaad = document.querySelector("#hozzaad");
+
+//Véletlenkép generátor
+function veletlenKep() {
+    kep = new Array()
+    kep[1] = "img/piros.png"
+    kep[2] = "img/feher.png"
+    kep[3] = "img/kek.png"
+    kep[4] = "img/rozsaszin.png"
+    kep[5] = "img/narancs.png"
+    kep[6] = "img/lila.png"
+    kep[7] = "img/sarga.png"
+    kep[8] = "img/zold.png"
+
+    ry = Math.floor(Math.random() * kep.length)
+    if (ry == 0)
+        ry = 1
+}
+
+veletlenKep();
+let megoldasElsoGolyo = document.createElement("img");
+megoldasElsoGolyo.src = kep[ry];
+megoldasElsoGolyoMezo.appendChild(megoldasElsoGolyo);
+megoldasElsoGolyo.hidden = true;
+veletlenKep();
+let megoldasMasodikGolyo = document.createElement("img");
+megoldasMasodikGolyo.src = kep[ry];
+megoldasMasodikGolyoMezo.appendChild(megoldasMasodikGolyo);
+megoldasMasodikGolyo.hidden = true;
+veletlenKep();
+let megoldasHarmadikGolyo = document.createElement("img");
+megoldasHarmadikGolyo.src = kep[ry];
+megoldasHarmadikGolyoMezo.appendChild(megoldasHarmadikGolyo);
+megoldasHarmadikGolyo.hidden = true;
+veletlenKep();
+let megoldasNegyedikGolyo = document.createElement("img");
+megoldasNegyedikGolyo.src = kep[ry];
+megoldasNegyedikGolyoMezo.appendChild(megoldasNegyedikGolyo);
+megoldasNegyedikGolyo.hidden = true;
+
+let megoldasTomb = [
+    megoldasElsoGolyo.src,
+    megoldasMasodikGolyo.src,
+    megoldasHarmadikGolyo.src,
+    megoldasNegyedikGolyo.src
+]
+
+function megoldasMutat() {
+    megoldasElsoGolyo.hidden = false;
+    megoldasMasodikGolyo.hidden = false;
+    megoldasHarmadikGolyo.hidden = false;
+    megoldasNegyedikGolyo.hidden = false;
+    rejt1.hidden = true;
+    rejt2.hidden = true;
+    rejt3.hidden = true;
+    let vesztettel = document.createElement("h3")
+    vesztettel.innerHTML = "Vesztettél!";
+    let ujJatekGomb = document.createElement("button");
+    ujJatekGomb.className = "btn btn-primary";
+    ujJatekGomb.innerHTML = "Új játék";
+    ujJatekGomb.addEventListener("click", function () {
+        location.reload();
+    });
+    hozzaad.appendChild(vesztettel)
+    hozzaad.appendChild(ujJatekGomb)
+}
