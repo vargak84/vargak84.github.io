b76d15714eed92bb28b06f2133f3c8dba424d527
vargak84 <einstein.xxx@gmail.com>
2020-09-15 08:41:32 +0200

    new

--- js/index.js
+++ js/index.js
@@ -2,17 +2,30 @@ let felhasznaloNev = document.querySelector("#felhasznaloNev");
 let udvozloMezo = document.querySelector("#neve");
 let okeGomb = document.querySelector("#okeGomb");
 
-$(document).ready(function () {
-  // Show the Modal on load
-  $("#myModal").modal("show");
-});
+let keresztNev = localStorage.keresztNev;
+
+if (keresztNev == undefined) {
+  $(document).ready(function () {
+    // Show the Modal on load
+    $("#myModal").modal("show");
+  });
+} else {
+  udvozloMezo.innerHTML = keresztNev;
+}
 
 function okeUdvozlet() {
-  udvozloMezo.innerHTML = felhasznaloNev.value;
+  
+  if (felhasznaloNev.value == "") {
+    felhasznaloNev.value = felhasznaloNev.placeholder
+    localStorage.keresztNev = felhasznaloNev.value;
+    udvozloMezo.innerHTML = localStorage.keresztNev;
+  } else {
+    udvozloMezo.innerHTML = felhasznaloNev.value;
+    localStorage.keresztNev = felhasznaloNev.value;
+  }
 };
 
-/*     let felhasznalo = prompt("Add meg a keresztneved!", "Vendég");
-    if (felhasznalo != null) {
-      document.getElementById("udvozlet").innerHTML =
-      "Üdvözöllek " + felhasznalo + "! Jó böngészést az oldalon!";
-  }; */
+function nevTorol() {
+  localStorage.clear(keresztNev);
+  location.reload();
+}
