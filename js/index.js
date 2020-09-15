3e65c9396f6556bed6973ab31f20ccfade101e75
vargak84 <einstein.xxx@gmail.com>
2020-09-15 09:46:51 +0200

    new

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let felhasznaloNev = document.querySelector("#felhasznaloNev");
-let udvozloMezo = document.querySelector("#neve");
+let neveMezo = document.querySelector("#neve");
 let okeGomb = document.querySelector("#okeGomb");
+let hibaUzenet = document.querySelector("#hibaUzenet")
 
+neveMezo.innerHTML = "Vendég";
 let keresztNev = localStorage.keresztNev;
 
 if (keresztNev == undefined) {
@@ -10,17 +12,20 @@ if (keresztNev == undefined) {
     $("#myModal").modal("show");
   });
 } else {
-  udvozloMezo.innerHTML = keresztNev;
+  neveMezo.innerHTML = keresztNev;
 }
 
 function okeUdvozlet() {
-  
   if (felhasznaloNev.value == "") {
-    felhasznaloNev.value = felhasznaloNev.placeholder
+    felhasznaloNev.value = felhasznaloNev.placeholder;
     localStorage.keresztNev = felhasznaloNev.value;
-    udvozloMezo.innerHTML = localStorage.keresztNev;
+    neveMezo.innerHTML = localStorage.keresztNev;
+  } else if(felhasznaloNev.value.indexOf(" ") == 0) {
+    keresztNev == undefined;
+    alert("Szóköz karakter nem lehet az első helyen!")
+    location.reload();
   } else {
-    udvozloMezo.innerHTML = felhasznaloNev.value;
+    neveMezo.innerHTML = felhasznaloNev.value;
     localStorage.keresztNev = felhasznaloNev.value;
   }
 };
